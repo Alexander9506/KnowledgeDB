5276adc50483b3bffeb6b22cdec5e02fc1ea6584
Alexander Körner <alexanderkoerner@hotmail.de>
2020-07-11 13:03:39 +0200

    tidy up the javascripts

---
+++ KnowledgeDB/wwwroot/source/GUICreation.js
@@ -0,0 +1,30 @@
+﻿function replaceButtonWithImage(button, imageClasses) {
+    const buttonParent = button.parentElement;
+    if (buttonParent != null) {
+        const image = document.createElement('i')
+        image.className = imageClasses
+
+        buttonParent.insertBefore(image, button);
+        buttonParent.removeChild(button);
+
+        return image;
+    }
+}
+
+function ChangeImage(image, imageClasses) {
+    if (image != null) {
+        image.className = imageClasses;
+    }
+}
+
+function createImageButton(imageClasses, buttonClasses, buttonTitle) {
+    let button = document.createElement('button');
+    let image = document.createElement('i');
+
+    image.className = imageClasses;
+    button.title = buttonTitle;
+    button.className = buttonClasses
+    button.appendChild(image);
+
+    return button;
+}

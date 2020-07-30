aef85a7f8b942a17887017d6c784e01ddfc1a622
Alexander Körner <alexanderkoerner@hotmail.de>
2020-07-30 08:53:48 +0200

    removed wwwroot\js

--- KnowledgeDB/wwwroot/js/Security.js
+++
@@ -1,3 +0,0 @@
-export function uuidv4() {
-    return ("" + 1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (parseFloat(c) ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> parseFloat(c) / 4).toString());
-}
